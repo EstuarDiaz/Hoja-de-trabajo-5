f77eadb08024e32e38e66c73c647b821af494c20
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-02-28 18:35:13 -0600

    Funcionamiento terminado
    Ya esta el control de los procesos, falta hacer las pruebas y las graficas.

--- HDT5.py
+++ HDT5.py
@@ -8,20 +8,53 @@ import simpy
 import random
 
 interval = 10
-numero_de_procesos = 10
+numero_de_procesos = 5
 tiempo_simulacion = 50
+random_seed = 17
+capacidad_RAM = 100
+cantidad_CPU = 1
+numero_instrucciones = 3
+max_size_proceso = 10
+max_size_ram = 10
 
-def proceso(env,nombre,tiempo_creacion,instrucciones):
+random.seed(random_seed) # fijar el inicio de random
+def proceso(env,nombre,tiempo_creacion,memoria_requerida,instrucciones):
     yield env.timeout(tiempo_creacion)
-    horaLlegada = env.now
-    print ('%s llega a las %f tiene %d instrucciones' % (nombre,horaLlegada,instrucciones))
+    print ('%s creado a las %f, tiene %d instrucciones' % (nombre,env.now,instrucciones))
+    # NEW
+    with RAM.get(memoria_requerida) as turnoRAM:
+        yield turnoRAM
+        print('%d unidades de RAM disponibles' % (RAM.level))
+        print ('%s ocupa %d de memoria a las %f' % (nombre,memoria_requerida,env.now))
+        
+    while instrucciones > 0:
+    # READY
+        with CPU.request() as turnoCPU:
+            print('%s esperando turno de CPU a las %f' % (nombre,env.now))
+            yield turnoCPU
+            
+    # RUNNING
+            instrucciones -= min(numero_instrucciones,instrucciones)
+            yield env.timeout(1) # el proceso ejecuta numero_instrucciones por unidad de tiempo
+            print('%s tiene %d instrucciones faltantes' % (nombre,instrucciones))
+            CPU.release(turnoCPU)
+        
+    # WAITING    
+        if instrucciones > 0:
+            if random.randint(1,2) == 1:
+                print('%s esperando I/O a las %f' % (nombre,env.now))
+                yield env.timeout(1) # simluar interrupcion de I/O
+
+    # TERMINATED
+    RAM.put(memoria_requerida)
+    print('%s terminado a las %f' % (nombre,env.now))
 
 env = simpy.Environment() #ambiente de simulación
-RAM = simpy.Container(env, init = 100, capacity = 100) # Cointainer para RAM
-CPU = simpy.Resource(env, capacity=1) # Resource para CPU
-random.seed(10) # fijar el inicio de random
+RAM = simpy.Container(env, init = capacidad_RAM, capacity = capacidad_RAM) # Cointainer para RAM
+CPU = simpy.Resource(env, capacity = cantidad_CPU) # Resource para CPU
 for i in range(numero_de_procesos):
-    env.process(proceso(env,'Proceso %d'%i,random.expovariate(1.0/interval),random.randint(1,10)))
+    env.process(proceso(env,'Proceso %d'%i,random.expovariate(1.0/interval),
+                            random.randint(1,max_size_ram),random.randint(1,max_size_proceso)))
 
 env.run(until = tiempo_simulacion)  #correr la simulación hasta el tiempo especificado
 
